876352d771b745aeeb1fcfbbbbf95bf5c53ca3b1
Kyle <k.c.bolay@gmail.com>
2024-04-13 20:31:24 -0500

    Fixing generic restriction for TResponse

--- src/Momentum.Repositories/Interfaces/ISearchRepository.cs
+++ src/Momentum.Repositories/Interfaces/ISearchRepository.cs
@@ -2,7 +2,7 @@ namespace Momentum.Repositories.Interfaces
 {
     public interface ISearchRepository<T, TPage, TRequest, TResponse>
         where TRequest : ISearchRequest<TPage>
-        where TResponse : ISearchResponse<TResponse, TPage>
+        where TResponse : ISearchResponse<T, TPage>
     {
         Task<TResponse> SearchAsync(TRequest request, CancellationToken token = default);
     } // end interface

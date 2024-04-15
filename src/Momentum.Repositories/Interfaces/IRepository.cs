e30f36bea4231473d969ba477fb1f4c07872bd9b
Kyle <k.c.bolay@gmail.com>
2024-04-15 11:14:18 -0500

    Improving IRepository method signatures

--- src/Momentum.Repositories/Interfaces/IRepository.cs
+++ src/Momentum.Repositories/Interfaces/IRepository.cs
@@ -2,10 +2,10 @@ namespace Momentum.Repositories.Interfaces
 {
     public interface IRepository<TId, T>
     {
-        Task<T> CreateAsync(TId id, CancellationToken token = default);
+        Task<T> CreateAsync(T id, CancellationToken token = default);
         Task DeleteAsync(T item, CancellationToken token = default);
         Task<T?> GetAsync(TId id, CancellationToken token = default);
-        Task<T> UpdateAsync(TId id, T item, CancellationToken token = default);
+        Task<T> UpdateAsync(T item, CancellationToken token = default);
     } // end interface
 
     public interface IRepository<T> : IRepository<Guid, T> {} // end interface
